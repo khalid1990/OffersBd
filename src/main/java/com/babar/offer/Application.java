8d30b8d1e53e927493f89d64bf46d0ae3f1fb286
babar <print.email016@gmail.com>
2018-01-31 08:31:09 +0600

    Minimum code for starting up a spring mvc project
    Spring boot web starter is used here.

---
+++ src/main/java/com/babar/offer/Application.java
@@ -0,0 +1,15 @@
+package com.babar.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author babar
+ * @since 1/31/18.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/babar/offer/controller/OfferController.java
@@ -0,0 +1,20 @@
+package com.babar.offer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author babar
+ * @since 2/2/18.
+ */
+@Controller
+@RequestMapping
+public class OfferController {
+
+    @ResponseBody
+    @RequestMapping
+    public String create() {
+        return "Create offers";
+    }
+}
