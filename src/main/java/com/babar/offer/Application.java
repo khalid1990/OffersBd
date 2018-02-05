52651bc186532e0e4804dacdbbe0a5528d30267f
babar <print.email016@gmail.com>
2018-02-05 17:12:41 +0600

    Added jsp rendering support by tomcat jasper inclusion

--- src/main/java/com/babar/offer/Application.java
+++ src/main/java/com/babar/offer/Application.java
@@ -2,6 +2,12 @@ package com.babar.offer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * @author babar
@@ -11,5 +17,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+
+        return bean;
     }
 }
--- src/main/java/com/babar/offer/controller/OfferController.java
+++ src/main/java/com/babar/offer/controller/OfferController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping
 public class OfferController {
 
-    @ResponseBody
     @RequestMapping
     public String create() {
-        return "Create offers";
+        return "test";
     }
 }
